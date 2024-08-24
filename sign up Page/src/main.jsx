8e28b7cbb6dd8ab261dd74515bd7b122d1a1f0b9
alianoosha93 <154692283+alianoosha93@users.noreply.github.com>
2024-08-23 23:55:41 -0700

    Done Form

---
+++ sign up Page/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+<Router>
+//     <App />
+</Router>
+  </React.StrictMode>,
+)
+
+
+
+
+// import React from 'react';  
+// import ReactDOM from 'react-dom';  
+// import { BrowserRouter as Router } from 'react-router-dom'; // Import Router  
+// import App from './App';  
+
+// ReactDOM.render(  
+//     <Router> {/* Wrap your App component with Router */}  
+//         <App />  
+//     </Router>,  
+//     document.getElementById('root')  
+// );
